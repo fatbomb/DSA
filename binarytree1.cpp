8cabb810a6ecc975d4f8e8d9f83a276eb80d4e41
fatbomb <joarderaniket@gmail.com>
2023-03-16 03:37:39 +0600

    temporay tree structue

---
+++ binarytree1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class node{
+    int data;
+    node* left;
+    node* right;
+    public:
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+
+};
