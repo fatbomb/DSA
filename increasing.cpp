1eea336317e2dd449635ea4d7ebb5d0b5612c817
fatbomb <joarderaniket@gmail.com>
2023-03-22 03:25:05 +0600

    made bamboo

---
+++ increasing.cpp
@@ -0,0 +1,26 @@
+#include "bst.hpp"
+
+
+node* newtree(node* root, node* t=NULL){
+    if(root==NULL){
+        return t;
+    }
+    node* temp= newtree(root->left,root);
+    root->left=NULL;
+    root->right= newtree(root->right,t);
+    return temp;
+}
+int main(){
+    BinaryTree t;
+    int n;
+    cin>>n;
+    while(n--){
+        int x;
+        cin>>x;
+        t.addnode(x);
+
+    }
+    t.root=newtree(t.root);
+    cout<<t.root->data<<endl;
+    t.preOrder();
+}
